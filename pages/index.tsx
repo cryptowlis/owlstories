5c6fec84a65be61cda0d27c05de38205757fc163
amalnathsathyan <amalnath.sn@pitsolutions.com>
2022-10-11 12:58:52 +0530

    updated ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <h1> Paniyedukk Kummaa !!</h1>
     </div>
   );
